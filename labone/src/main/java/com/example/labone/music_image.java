0456846826e41a766e501c082d037a1b2e3b72ba
Alexander <AlexanderBely98@gmail.com>
2018-12-02 17:55:05 +0300

    Initial commit

---
+++ labone/src/main/java/com/example/labone/music_image.java
@@ -0,0 +1,15 @@
+package com.example.labone;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class music_image extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.music_image, container, false);
+    }
+}
